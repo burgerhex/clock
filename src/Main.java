79ec3075dc3a1b04535057dd7a0519aeddfd2baf
burgerhex <aviv.shai@gmail.com>
2019-01-27 19:03:04 -0800

    completed functioning main program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+
 public class Main {
 
-    
+    private static final JFrame frame = new JFrame();
+    private static final ClockPanel clock = new ClockPanel();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        SwingUtilities.invokeLater(Main::createAndShowGUI);
+
+        while (true) {
+            clock.tick();
+        }
+    }
+
+    private static void createAndShowGUI() {
+        frame.setTitle("Clock");
+        frame.setResizable(false);
+        frame.setAlwaysOnTop(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(clock);
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 
 }
